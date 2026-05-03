a5ce8bc500c57c9af3845378263f2165cb0d5aa2
dilshanmethsara <you@example.com>
2026-05-03 08:02:34 +0530

    Fix module export syntax - use CommonJS for Vercel compatibility

--- api/send-email.js
+++ api/send-email.js
@@ -109,7 +109,7 @@ const getPurchaseEmailTemplate = (userName, orderDetails) => {
 };
 
 // Main handler
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
